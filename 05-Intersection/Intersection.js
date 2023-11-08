a9a5ec2d17fe7a54a1b6a0ea1b538de0e98586f7
PC1 <kevsalomdev@gmail.com>
2023-11-07 23:49:12 -0400

    5-7 done

--- 05-Intersection/Intersection.js
+++ 05-Intersection/Intersection.js
@@ -2,13 +2,11 @@ function intersection(arr1, arr2) {
  
   const result = [];
   arr1.forEach(num => {
-    if (arr2.includes(num)) {
-      result.push(num);
-    }
+    if (arr2.includes(num)) result.push(num);
   });
   return result;
 }
 
-console.log(intersection([1,3,5,7,10], [2,3,6,8,10,20]))
+
 
 module.exports = intersection;
--- 07-SpyOn/SpyOn.js
+++ 07-SpyOn/SpyOn.js
@@ -1,5 +1,29 @@
 function spyOn(fn) {
-  // Tu código aquí
+
+  let count = 0
+  let memoryCallWith = []
+  let memoryReturned = []
+
+  const spy = (...arg)=>{
+    memoryCallWith = [...new Set([...memoryCallWith, ...arg])]
+    count += 1;
+    const value = fn(...arg);
+    memoryReturned.push(value)
+    return  value
+  }
+
+  spy.getCallCount = ()=>{
+    return count
+  }
+  spy.wasCalledWith = (val)=>{
+    return memoryCallWith.includes(val)
+  }
+  spy.returned = (val)=>{
+    return memoryReturned.includes(val)
+  }
+  return spy
 }
+
+
 
 module.exports = spyOn;
